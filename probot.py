c2b2acc85d12f1e02b1a4bb02e84637ec523f333
tmupper <129107684+tupper6261@users.noreply.github.com>
2023-07-13 10:39:11 -0400

    Update probot.py

--- probot.py
+++ probot.py
@@ -10,55 +10,36 @@ load_dotenv()
 DATABASE_TOKEN = os.getenv('DATABASE_TOKEN')
 BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
 
-guild_id = 1101580614945222708
-channel_id = 1129038551066103959
-roles = {
-    "Artist": 1107785958746771538,
-    "Coder": 1107786089932013709,
-    "Collector": 1107786213575893012
-}
+class MyView(discord.ui.View):  # Create a class called MyView that subclasses discord.ui.View
+    @discord.ui.button(label="Artist", style=discord.ButtonStyle.primary)  # Create a button with the label "Artist" with color Blurple
+    async def artist_button_callback(self, button, interaction):
+        role = discord.utils.get(interaction.guild.roles, id=1107785958746771538)
+        await self.toggle_role(interaction, role)
 
-# Set up the bot with the proper intents to read message content and reactions
-intents = discord.Intents.default()
-intents.message_content = True
-intents.reactions = True
-intents.members = True
-bot = commands.Bot(command_prefix="!", intents=intents)
+    @discord.ui.button(label="Coder", style=discord.ButtonStyle.primary)  # Create a button with the label "Coder" with color Blurple
+    async def coder_button_callback(self, button, interaction):
+        role = discord.utils.get(interaction.guild.roles, id=1107786089932013709)
+        await self.toggle_role(interaction, role)
+
+    @discord.ui.button(label="Collector", style=discord.ButtonStyle.primary)  # Create a button with the label "Collector" with color Blurple
+    async def collector_button_callback(self, button, interaction):
+        role = discord.utils.get(interaction.guild.roles, id=1107786213575893012)
+        await self.toggle_role(interaction, role)
+
+    @staticmethod
+    async def toggle_role(interaction, role):
+        if role in interaction.user.roles:
+            await interaction.user.remove_roles(role)
+            await interaction.response.send_message(f"Removed role {role.name}", ephemeral=True)
+        else:
+            await interaction.user.add_roles(role)
+            await interaction.response.send_message(f"Added role {role.name}", ephemeral=True)
 
 @bot.event
 async def on_ready():
-    guild = discord.utils.get(bot.guilds, id=guild_id)
-    channel = discord.utils.get(guild.channels, id=channel_id)
-    await channel.send(
-        "Choose your role:",
-        components=[
-            [
-                Button(style=discord.ButtonStyle.green, label=name)
-                for name in roles.keys()
-            ]
-        ]
-    )
-
-@bot.event
-async def on_button_click(res):
-    guild = discord.utils.get(bot.guilds, id=guild_id)
-    role_id = roles[res.component.label]
-    role = discord.utils.get(guild.roles, id=role_id)
-    member = res.guild.get_member(res.user.id)
-
-    if role in member.roles:
-        await member.remove_roles(role)
-        await res.respond(
-            type=InteractionType.ChannelMessageWithSource,
-            content=f"Removed role {res.component.label}",
-            ephemeral=True
-        )
-    else:
-        await member.add_roles(role)
-        await res.respond(
-            type=InteractionType.ChannelMessageWithSource,
-            content=f"Added role {res.component.label}",
-            ephemeral=True
-        )
+    print(f"We have logged in as {bot.user}")
+    guild = discord.utils.get(bot.guilds, id=1101580614945222708)
+    channel = discord.utils.get(guild.channels, id=1129038551066103959)
+    await channel.send("Choose your role:", view=MyView())
 
 bot.run(BOT_TOKEN)
