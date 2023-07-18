f8dabcfb89d88de2e0bc3f7751eb8f0639ef933e
tmupper <129107684+tupper6261@users.noreply.github.com>
2023-07-18 15:21:27 -0400

    Update probot.py

--- probot.py
+++ probot.py
@@ -12,10 +12,10 @@ import json
 
 load_dotenv()
 
-DATABASE_TOKEN = os.getenv('DATABASE_TOKEN')
-BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
-ALCHEMY_API_KEY = os.getenv('ALCHEMY_API_KEY')
-RESERVOIR_API_KEY = os.getenv('RESERVOIR_API_KEY')
+DATABASE_TOKEN = 'postgres://yezaufigplmbrj:daa40d1942c2a9bc98258d57d3c6835b989d25aae748cfb199526dca7da06a66@ec2-34-228-248-175.compute-1.amazonaws.com:5432/db6a6eqkldihes' #os.getenv('DATABASE_TOKEN')
+BOT_TOKEN = 'MTEyODcxNTMwOTM1Mjc1OTM0Nw.G5joCg.UhUbG7rW-R3Akwzr4df-YjiuUcxgeq63nbBecY' #os.getenv('DISCORD_BOT_TOKEN')
+ALCHEMY_API_KEY = 'A1iH19r0eHoN-M8TubwLZ1iS-F47WG-G' #os.getenv('ALCHEMY_API_KEY')
+RESERVOIR_API_KEY = '7772cfc3-8666-55e6-ad93-dc665138f1ac' #os.getenv('RESERVOIR_API_KEY')
 prohibitionContract = "0x47A91457a3a1f700097199Fd63c039c4784384aB"
 
 # Set up web3
@@ -68,8 +68,8 @@ async def on_ready():
     guild = discord.utils.get(bot.guilds, id=1101580614945222708)
     channel = discord.utils.get(guild.channels, id=1129038551066103959)
     message_id = 1129129885928005874
-    #message = await channel.fetch_message(message_id)
-    #await message.edit(content="Tap the below buttons to add/remove the corresponding role:", view=MyView())
+    message = await channel.fetch_message(message_id)
+    await message.edit(content="Tap the below buttons to add/remove the corresponding role:", view=MyView())
     while True:
         await track()
         await asyncio.sleep(60)
@@ -84,7 +84,6 @@ async def track():
     command = "select * from globalvariables where name = 'prohibition_latest_mint_block'"
     cur.execute(command)
     results = cur.fetchall()
-    print (results)
     from_block = results[0][1]
     cur.close()
     conn.commit()
@@ -94,7 +93,8 @@ async def track():
     #contract deploy block = 105291124
 
     from_block = int(from_block)
-    current_block = w3.eth.blockNumber
+    current_block = w3.eth.block_number
+
 
     # Filter for Mint events
     mint_events = []
