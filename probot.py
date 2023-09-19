32badf48c3121f7025e1af55a292aaaf5cec8029
tmupper <129107684+tupper6261@users.noreply.github.com>
2023-09-19 16:46:42 -0400

    Update probot.py

--- probot.py
+++ probot.py
@@ -264,6 +264,8 @@ async def updateVoteMessage(vote_id, number_of_verified_artists):
     #See if a quorum has been reached
     total_vote_percent = round(((float(votes_for) + float(votes_against))/float(number_of_verified_artists)), 4) * 100
     if total_vote_percent < number_of_verified_artists * VERIFICATION_QUORUM:
+        print (total_vote_percent)
+        print (number_of_verified_artists * VERIFICATION_QUORUM)
         quorum_reached = False
     else:
         quorum_reached = True
