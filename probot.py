24b46e2e292531bb31dc03605dbf4eec4a18eb47
tmupper <129107684+tupper6261@users.noreply.github.com>
2023-09-21 11:51:09 -0400

    Update probot.py

--- probot.py
+++ probot.py
@@ -281,7 +281,7 @@ async def updateVoteMessage(vote_id, number_of_verified_artists):
         message_content = "\n\n**Current Vote Status:**"
         message_content += "\n{0} votes for".format(votes_for)
         message_content += "\n{0} votes against".format(votes_against)
-        message_content += "\n{0}% of Verified Artists have voted - a quorum has ".format(str(total_vote_percent))
+        message_content += "\n{0}% of Verified Artists have voted - a quorum has ".format(str(total_vote_percent*100))
         if not quorum_reached:
             message_content += "not "
         message_content += "been reached"
