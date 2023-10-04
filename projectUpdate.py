9608772237608039d8e7cb60b577a810f93a5beb
tmupper <129107684+tupper6261@users.noreply.github.com>
2023-10-04 15:27:20 -0400

    Update projectUpdate.py

--- projectUpdate.py
+++ projectUpdate.py
@@ -10,62 +10,70 @@ ALCHEMY_MAINNET_API_KEY = os.environ.get('ALCHEMY_MAINNET_API_KEY')
 ARBISCAN_API_KEY = os.environ.get('ARBISCAN_API_KEY')
 DATABASETOKEN = os.environ.get('DATABASETOKEN')
 
+#Get the prohibition contract ABI
 response = requests.get("https://api.arbiscan.io/api?module=contract&action=getabi&address=" + prohibitionContract + "&apikey=" + ARBISCAN_API_KEY)
 PROHIBITION_CONTRACT_ABI = json.loads(response.text)['result']
 
+#Connect to alchemy
 w3 = Web3(HTTPProvider('https://arb-mainnet.g.alchemy.com/v2/'+ALCHEMY_MAINNET_API_KEY))
 
+#Load the prohibition contract
 contract = w3.eth.contract(address=prohibitionContract, abi=PROHIBITION_CONTRACT_ABI)
 
+#See how many projects currently exist
 nextProjectId = contract.functions.nextProjectId().call()
 
+#Connect to Postgres
 conn = psycopg2.connect(DATABASETOKEN, sslmode='require')
 cur = conn.cursor()
 
+#Go through all the existing projects
 for projectID in range(0,nextProjectId):
-    
+    #Grab some pertinent info from the projectStateData function
     result = contract.functions.projectStateData(projectID).call()
-
-    # Extract the returned values
+    #Extract the returned values
     invocations, maxInvocations, active, paused, completedTimestamp, locked = result
-
+    #If the project isn't active, we don't need to worry about it
     if active:
+        #Grab some more info from the projectDetails function
         result = contract.functions.projectDetails(projectID).call()
-
+        #Extract the returned values
         projectName, artist, description, website, license = result
-
+        #Get the prohibition minter contract address
         minterContractAddress = contract.functions.minterContract().call()
-
+        #Get the minter contract ABI
         response = requests.get("https://api.arbiscan.io/api?module=contract&action=getabi&address=" + minterContractAddress + "&apikey=" + ARBISCAN_API_KEY)
         minterContractABI = json.loads(response.text)['result']
-
+        #Load the minter contract
         minterContract = w3.eth.contract(address=minterContractAddress, abi=minterContractABI)
-
+        #Now get this specific project's minter contract address
         projectMinterContractAddress = minterContract.functions.getMinterForProject(projectID).call()
-
+        #Get the project's minter contract ABI
         response = requests.get("https://api.arbiscan.io/api?module=contract&action=getabi&address=" + projectMinterContractAddress + "&apikey=" + ARBISCAN_API_KEY)
         projectMinterContractABI = json.loads(response.text)['result']
-
+        #Load the project's minter contract
         projectMinterContract = w3.eth.contract(address=projectMinterContractAddress, abi=projectMinterContractABI)
-
+        #Get a few more pieces of info from the getPriceInfo function on the minter contract
         result = projectMinterContract.functions.getPriceInfo(projectID).call()
-
+        #Extract the returned values
         isConfigured, tokenPriceInWei, currencySymbol, currencyAddress = result
-
+        #Convert the token price into standard readable format
         readableTokenPrice = float(tokenPriceInWei)/1000000000000000000
-
+        #Set the project url and image url
         projectImage = "https://prohibition-arbitrum.s3.amazonaws.com/" + str(projectID * 1000000) + ".png"
         url = "https://prohibition.art/api/project/0x47A91457a3a1f700097199Fd63c039c4784384aB-"+str(projectID)
         response = requests.get(url)
+        #Make sure the project exists on the prohibition platform
         if response.text == '{"code":"NOT_FOUND","error":"Project not found"}':
             continue
         data = json.loads(response.text)
         projectURL = "https://prohibition.art/project/" + data['slug']
 
+        #See if there are still tokens available to mint
         stillMinting = True
         if invocations >= maxInvocations:
             stillMinting = False
-
+        #Upsert the SQL table
         cur.execute("""
             INSERT INTO prohibition_projects (
                 project_ID, 
@@ -94,14 +102,6 @@ for projectID in range(0,nextProjectId):
                 project_url = EXCLUDED.project_url;
         """, (projectID, stillMinting, invocations, maxInvocations, projectName, artist, projectMinterContractAddress, readableTokenPrice, projectImage, projectURL))
         conn.commit()
-        
-        print (projectName)
-        print (projectID)
-        print (str(invocations) + "/" + str(maxInvocations))
-        print (str(readableTokenPrice) + currencySymbol)
-        print (projectImage)
-        print (projectURL)
-        print ("-----")
         
 cur.close()
 conn.commit()
