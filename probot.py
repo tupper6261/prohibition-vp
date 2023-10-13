7ad42862a0849a4bd49b6e1416616845c89dcc7f
tmupper <129107684+tupper6261@users.noreply.github.com>
2023-10-13 15:35:32 -0400

    Update probot.py

--- probot.py
+++ probot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands, tasks
 from discord.ui import Button, View, Select
-from discord.commands import Option
+from discord.commands import Option, OptionChoice
 import os
 from dotenv import load_dotenv
 import asyncio
@@ -208,102 +208,109 @@ async def updateRoles():
     await message.edit(content="Tap the below buttons to add/remove the corresponding role:", view=MyView())
 
 async def updateProjects():
-    #Get the prohibition contract ABI
-    response = requests.get("https://api.arbiscan.io/api?module=contract&action=getabi&address=" + prohibitionContract + "&apikey=" + ARBISCAN_API_KEY)
-    PROHIBITION_CONTRACT_ABI = json.loads(response.text)['result']
+    try:
+        #Get the prohibition contract ABI
+        response = requests.get("https://api.arbiscan.io/api?module=contract&action=getabi&address=" + prohibitionContract + "&apikey=" + ARBISCAN_API_KEY)
+        PROHIBITION_CONTRACT_ABI = json.loads(response.text)['result']
 
-    #Connect to alchemy
-    w3 = Web3(HTTPProvider('https://arb-mainnet.g.alchemy.com/v2/'+ALCHEMY_MAINNET_API_KEY))
+        #Connect to alchemy
+        w3 = Web3(HTTPProvider('https://arb-mainnet.g.alchemy.com/v2/'+ALCHEMY_MAINNET_API_KEY))
 
-    #Load the prohibition contract
-    contract = w3.eth.contract(address=prohibitionContract, abi=PROHIBITION_CONTRACT_ABI)
+        #Load the prohibition contract
+        contract = w3.eth.contract(address=prohibitionContract, abi=PROHIBITION_CONTRACT_ABI)
 
-    #See how many projects currently exist
-    nextProjectId = contract.functions.nextProjectId().call()
+        #See how many projects currently exist
+        nextProjectId = contract.functions.nextProjectId().call()
 
-    #Connect to Postgres
-    conn = psycopg2.connect(DATABASE_TOKEN, sslmode='require')
-    cur = conn.cursor()
+        #Connect to Postgres
+        conn = psycopg2.connect(DATABASE_TOKEN, sslmode='require')
+        cur = conn.cursor()
 
-    #Go through all the existing projects
-    for projectID in range(0,nextProjectId):
-        #Grab some pertinent info from the projectStateData function
-        result = contract.functions.projectStateData(projectID).call()
-        #Extract the returned values
-        invocations, maxInvocations, active, paused, completedTimestamp, locked = result
-        #If the project isn't active, we don't need to worry about it
-        if active:
-            #Grab some more info from the projectDetails function
-            result = contract.functions.projectDetails(projectID).call()
+        #Go through all the existing projects
+        for projectID in range(0,nextProjectId):
+            #Grab some pertinent info from the projectStateData function
+            result = contract.functions.projectStateData(projectID).call()
             #Extract the returned values
-            projectName, artist, description, website, license = result
-            #Get the prohibition minter contract address
-            minterContractAddress = contract.functions.minterContract().call()
-            #Get the minter contract ABI
-            response = requests.get("https://api.arbiscan.io/api?module=contract&action=getabi&address=" + minterContractAddress + "&apikey=" + ARBISCAN_API_KEY)
-            minterContractABI = json.loads(response.text)['result']
-            #Load the minter contract
-            minterContract = w3.eth.contract(address=minterContractAddress, abi=minterContractABI)
-            #Now get this specific project's minter contract address
-            projectMinterContractAddress = minterContract.functions.getMinterForProject(projectID).call()
-            #Get the project's minter contract ABI
-            response = requests.get("https://api.arbiscan.io/api?module=contract&action=getabi&address=" + projectMinterContractAddress + "&apikey=" + ARBISCAN_API_KEY)
-            projectMinterContractABI = json.loads(response.text)['result']
-            #Load the project's minter contract
-            projectMinterContract = w3.eth.contract(address=projectMinterContractAddress, abi=projectMinterContractABI)
-            #Get a few more pieces of info from the getPriceInfo function on the minter contract
-            result = projectMinterContract.functions.getPriceInfo(projectID).call()
-            #Extract the returned values
-            isConfigured, tokenPriceInWei, currencySymbol, currencyAddress = result
-            #Convert the token price into standard readable format
-            readableTokenPrice = float(tokenPriceInWei)/1000000000000000000
-            #Set the project url and image url
-            projectImage = "https://prohibition-arbitrum.s3.amazonaws.com/" + str(projectID * 1000000) + ".png"
-            url = "https://prohibition.art/api/project/0x47A91457a3a1f700097199Fd63c039c4784384aB-"+str(projectID)
-            response = requests.get(url)
-            #Make sure the project exists on the prohibition platform
-            if response.text == '{"code":"NOT_FOUND","error":"Project not found"}':
-                continue
-            data = json.loads(response.text)
-            projectURL = "https://prohibition.art/project/" + data['slug']
+            invocations, maxInvocations, active, paused, completedTimestamp, locked = result
+            #If the project isn't active, we don't need to worry about it
+            if active:
+                #Grab some more info from the projectDetails function
+                result = contract.functions.projectDetails(projectID).call()
+                #Extract the returned values
+                projectName, artist, description, website, license = result
+                #Get the prohibition minter contract address
+                minterContractAddress = contract.functions.minterContract().call()
+                #Get the minter contract ABI
+                response = requests.get("https://api.arbiscan.io/api?module=contract&action=getabi&address=" + minterContractAddress + "&apikey=" + ARBISCAN_API_KEY)
+                minterContractABI = json.loads(response.text)['result']
+                #Load the minter contract
+                minterContract = w3.eth.contract(address=minterContractAddress, abi=minterContractABI)
+                #Now get this specific project's minter contract address
+                projectMinterContractAddress = minterContract.functions.getMinterForProject(projectID).call()
+                #Get the project's minter contract ABI
+                response = requests.get("https://api.arbiscan.io/api?module=contract&action=getabi&address=" + projectMinterContractAddress + "&apikey=" + ARBISCAN_API_KEY)
+                projectMinterContractABI = json.loads(response.text)['result']
+                #Load the project's minter contract
+                projectMinterContract = w3.eth.contract(address=projectMinterContractAddress, abi=projectMinterContractABI)
+                #Get a few more pieces of info from the getPriceInfo function on the minter contract
+                result = projectMinterContract.functions.getPriceInfo(projectID).call()
+                #Extract the returned values
+                isConfigured, tokenPriceInWei, currencySymbol, currencyAddress = result
+                #Convert the token price into standard readable format
+                readableTokenPrice = float(tokenPriceInWei)/1000000000000000000
+                #Set the project url and image url
+                projectImage = "https://prohibition-arbitrum.s3.amazonaws.com/" + str(projectID * 1000000) + ".png"
+                url = "https://prohibition.art/api/project/0x47A91457a3a1f700097199Fd63c039c4784384aB-"+str(projectID)
+                response = requests.get(url)
+                #Make sure the project exists on the prohibition platform
+                if response.text == '{"code":"NOT_FOUND","error":"Project not found"}':
+                    continue
+                data = json.loads(response.text)
+                projectURL = "https://prohibition.art/project/" + data['slug']
 
-            #See if there are still tokens available to mint
-            stillMinting = True
-            if invocations >= maxInvocations:
-                stillMinting = False
-            #Upsert the SQL table
-            cur.execute("""
-                INSERT INTO prohibition_projects (
-                    project_ID, 
-                    is_active, 
-                    invocations, 
-                    max_invocations, 
-                    project_name, 
-                    project_artist, 
-                    minter_contract, 
-                    project_price, 
-                    project_image, 
-                    project_url
-                ) VALUES (
-                    %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
-                )
-                ON CONFLICT (project_ID) 
-                DO UPDATE SET 
-                    is_active = EXCLUDED.is_active,
-                    invocations = EXCLUDED.invocations,
-                    max_invocations = EXCLUDED.max_invocations,
-                    project_name = EXCLUDED.project_name,
-                    project_artist = EXCLUDED.project_artist,
-                    minter_contract = EXCLUDED.minter_contract,
-                    project_price = EXCLUDED.project_price,
-                    project_image = EXCLUDED.project_image,
-                    project_url = EXCLUDED.project_url;
-            """, (projectID, stillMinting, invocations, maxInvocations, projectName, artist, projectMinterContractAddress, readableTokenPrice, projectImage, projectURL))
-            conn.commit()
-            
-    cur.close()
-    conn.commit()
-    conn.close()   
+                #See if there are still tokens available to mint
+                stillMinting = True
+                if invocations >= maxInvocations:
+                    stillMinting = False
+                #Upsert the SQL table
+                cur.execute("""
+                    INSERT INTO prohibition_projects (
+                        project_ID, 
+                        is_active, 
+                        invocations, 
+                        max_invocations, 
+                        project_name, 
+                        project_artist, 
+                        minter_contract, 
+                        project_price, 
+                        project_image, 
+                        project_url
+                    ) VALUES (
+                        %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
+                    )
+                    ON CONFLICT (project_ID) 
+                    DO UPDATE SET 
+                        is_active = EXCLUDED.is_active,
+                        invocations = EXCLUDED.invocations,
+                        max_invocations = EXCLUDED.max_invocations,
+                        project_name = EXCLUDED.project_name,
+                        project_artist = EXCLUDED.project_artist,
+                        minter_contract = EXCLUDED.minter_contract,
+                        project_price = EXCLUDED.project_price,
+                        project_image = EXCLUDED.project_image,
+                        project_url = EXCLUDED.project_url;
+                """, (projectID, stillMinting, invocations, maxInvocations, projectName, artist, projectMinterContractAddress, readableTokenPrice, projectImage, projectURL))
+                conn.commit()
+                
+        cur.close()
+        conn.commit()
+        conn.close()   
+    #If an error occurs, we're gonna log it and pause for a minute, then try again
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        line_number = exc_tb.tb_lineno
+        print(f"An error occurred: {e} at line {line_number}")
+        await asyncio.sleep(60)
 
 async def updateVotes():
     #Get the current active votes
@@ -439,7 +446,7 @@ async def project(ctx):
 
 #Slash command to discover a prohibition project
 @bot.slash_command(guild_ids=[PROHIBITION_GUILD_ID], description="Discover a new project on the Prohibition platform")
-async def discover(ctx, active: discord.OptionChoice(["Only Active Projects", "Any Project"], "Show a random iteration of an actively-minting project, or from any project?") = "Any Project"):
+async def discover(ctx, active: discord.OptionChoice(str, "Show a random iteration of an actively-minting project, or from any project?", choices = ["Only Active Projects", "Any Project"]) = "Any Project"):
     conn = psycopg2.connect(DATABASE_TOKEN, sslmode='require')
     cur = conn.cursor()
     if active == "Only Active Projects":
