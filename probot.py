ce487e7775d881197cae7b493d59691eb4c1b459
tmupper <129107684+tupper6261@users.noreply.github.com>
2023-10-18 14:15:47 -0400

    Update probot.py

--- probot.py
+++ probot.py
@@ -82,8 +82,6 @@ for i in results:
     if len(artist_name) > 100:
         artist_name = artist_name[:96] + "..."
     PROHIBITION_PROJECT_NAMES.append(project_name)
-    print (len(PROHIBITION_PROJECT_NAMES))
-    print (project_name)
     if artist_name not in PROHIBITION_ARTISTS:
         PROHIBITION_ARTISTS.append(artist_name)
 
@@ -506,7 +504,7 @@ async def project(ctx, projectname: discord.Option(str, autocomplete = discord.u
 
 #Slash command to display an invocation from a specified artist
 @bot.slash_command(guild_ids=[PROHIBITION_GUILD_ID], description="Display an invocation of a minted project by a specified artist")
-async def artist(ctx, artistname: discord.Option(str, autocomplete = discord.utils.basic_autocomplete(PROHIBITION_ARTISTS))):
+async def artist(ctx, artistname: discord.Option(str, autocomplete = PROHIBITION_ARTISTS)):
     conn = psycopg2.connect(DATABASE_TOKEN, sslmode='require')
     cur = conn.cursor()
     cur.execute("select * from prohibition_projects where project_artist = '{}'".format(artistname))
